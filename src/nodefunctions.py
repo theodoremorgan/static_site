89ad88c1264e051b356ecd0a7b5a874404870a4a
theo <theodore.joseph.morgan@gmail.com>
2024-09-12 22:21:11 +0100

    created tests for image and link node splits. functions to be completed

--- src/nodefunctions.py
+++ src/nodefunctions.py
@@ -1,3 +1,4 @@
+import re
 from textnode import TextNode
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
@@ -9,4 +10,17 @@ def split_node_by_delimiter(single_node, delimiter, text_type):
         lambda x, y: TextNode(x, y)
         , single_node.text.split(delimiter,2)
         , [single_node.text_type, text_type, single_node.text_type]))
+
+def extract_markdown_images(text):
+    return re.findall( r"!\[(.*?)\]\((.*?)\)" , text)
+    
+def extract_markdown_links(text):
+    return re.findall(r"(?<!!)\[(.*?)\]\((.*?)\)", text)
+
+def split_nodes_image(old_nodes):
+    pass
+
+def split_nodes_link(old_nodes):
+    pass
+
 
--- src/test_nodefunctions.py
+++ src/test_nodefunctions.py
@@ -1,5 +1,5 @@
 import unittest
-from nodefunctions import split_nodes_delimiter
+from nodefunctions import split_nodes_delimiter, extract_markdown_images, extract_markdown_links, split_nodes_image, split_nodes_link
 from textnode import TextNode
 
 class TestNodeFunctions(unittest.TestCase):
@@ -63,6 +63,44 @@ class TestNodeFunctions(unittest.TestCase):
                 TextNode(" word", "text"),
                 ]
         self.assertEqual(actual_nodes, expected_nodes)
+
+    def test_extract_markdown_images(self):
+        text = "This is text with a image ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg) and a red herring [to boot dev](https://www.boot.dev)"
+        expected_images = [("rick roll", "https://i.imgur.com/aKaOqIh.gif"), ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg")]
+        actual_images = extract_markdown_images(text)
+        self.assertEqual(actual_images, expected_images)
+
+    def test_extract_markdown_links(self):
+        text = "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev) and a red herring ![rick roll](https://i.imgur.com/aKaOqIh.gif)"
+        expected_links = [("to boot dev", "https://www.boot.dev"), ("to youtube", "https://www.youtube.com/@bootdotdev")]   
+        actual_links = extract_markdown_links(text)
+        self.assertEqual(actual_links, expected_links)
+
+    def test_split_nodes_link(self):
+
+        node = TextNode("This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev) and a red herring ![rick roll](https://i.imgur.com/aKaOqIh.gif)", "text",)
+        actual_new_nodes = split_nodes_link([node])
+        expected_new_nodes = [
+                TextNode("This is text with a link ", "text"),
+                TextNode("to boot dev", "link", "https://www.boot.dev"),
+                TextNode(" and ", "text"),
+                TextNode("to youtube", "link", "https://www.youtube.com/@bootdotdev"),
+                TextNode(" and a red herring ![rick roll](https://i.imagur.com/aKa0qIh.gif)", "text",),
+                ]
+        self.assertEqual(actual_new_nodes, expected_new_nodes)
+
+    def test_split_nodes_image(self): 
+        node = TextNode("This is text with a image ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg) and a red herring [to boot dev](https://www.boot.dev)", "text",)
+        actual_new_nodes = split_nodes_image([node])
+        expected_new_nodes = [
+                TextNode("This is text with a image ", "text",),
+                TextNode("rick roll", "link", "https://i.imgur.com/aKa0qIh.gif"),
+                TextNode(" and ", "text"),
+                TextNode("obi wan", "link", "https://i.imgur.com/fJRm4Vk.jpeg"),
+                TextNode("and a red herring [to boot dev](https://www.boot.dev)", "text",),
+                ]
+        self.assertEqual(actual_new_nodes, expected_new_nodes)
+
 
 if __name__ == "__main__":
     unittest.main()
